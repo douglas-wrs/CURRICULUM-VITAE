620a4005e75beccae5127247bfddb98ad4e8961e
Douglas Winston <dsoares@gruporecovery.com>
2023-01-11 14:52:05 -0300

    page title

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 import pandas as pd
 import plotly.express as px
 
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide", page_title="Douglas Winston")
 
 st.title('Douglas Winston')
 st.markdown('#### Data Product Manager')
